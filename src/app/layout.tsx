0420bae1ae6c9c5e7ad9b9f60bbfe1d160065987
allankildare <allankildaredev@gmail.com>
2024-05-25 14:39:06 -0300

    feat: favicon defined in metadata and prettier format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,30 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Mira Frontend Challenge",
-  description: "This web application is a frontend challenge made for Mira",
-};
+  title: 'Mira Frontend Challenge',
+  description: 'This web application is a frontend challenge made for Mira',
+  icons: [
+    {
+      rel: 'shortcut icon',
+      type: 'image/x-icon',
+      sizes: '32x32',
+      url: '/images/favicon.gif',
+    },
+  ],
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
