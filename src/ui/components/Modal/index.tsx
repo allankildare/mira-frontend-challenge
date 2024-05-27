917dbd977ec376d3c1529d371ca6368a02e67e00
allankildare <allankildaredev@gmail.com>
2024-05-27 01:06:56 -0300

    feat: adding modal component

---
+++ src/ui/components/Modal/index.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import { ReactNode } from 'react'
+
+interface ModalProps {
+  children: ReactNode
+  title: string
+  closeFunc: () => void
+}
+
+export function Modal({ children, title, closeFunc }: ModalProps) {
+  return (
+    <div className="modal-wrapper">
+      <div className="modal-view">
+        <div className="flex justify-between items-center w-full mb-2">
+          <h1 className="card-mira-title text-xl">{title}</h1>
+          <button
+            onClick={(event) => {
+              event.preventDefault()
+              closeFunc()
+            }}
+          >
+            <Image
+              src="/icons/xmark-solid.svg"
+              alt="Close Order detail edition"
+              width={16}
+              height={16}
+            />
+          </button>
+        </div>
+        <div className="w-full">{children}</div>
+      </div>
+    </div>
+  )
+}
