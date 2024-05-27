bf5cec0e168b7fc2bcbbe81ae22de6ebd7b4c21f
allankildare <allankildaredev@gmail.com>
2024-05-27 01:03:15 -0300

    feat: data context

---
+++ src/contexts/dataContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import data from '~/data/index.json'
+
+export const DataContext = createContext(null)
+
+export function DataContextProvider({ children }: { children: ReactNode }) {
+  const [currentData, setCurrentData] = useState(data)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('mira-data') === null) setCurrentData(data)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('mira-data', JSON.stringify(currentData))
+  }, [currentData])
+
+  function deleteMedicine(index: number) {
+    const filteredOTC = currentData.OTC.filter(
+      (_, medIndex) => medIndex !== index
+    )
+    setCurrentData({ ...currentData, OTC: filteredOTC })
+  }
+  return (
+    <DataContext.Provider value={{ currentData, deleteMedicine }}>
+      {children}
+    </DataContext.Provider>
+  )
+}
