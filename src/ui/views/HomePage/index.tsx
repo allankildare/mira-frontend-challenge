7ccdf44f1bb360cabd12851d8725472b6dae9334
allankildare <allankildaredev@gmail.com>
2024-05-27 01:11:42 -0300

    feat: adding section to home page to render navigator and provider views

--- src/ui/views/HomePage/index.tsx
+++ src/ui/views/HomePage/index.tsx
@@ -1,10 +1,14 @@
-import { OrderDetails } from '~/ui/components'
+import { NavigatorAndProviderView, OrderDetails } from '~/ui/components'
 
 export function HomePage() {
   return (
     <>
       <section>
         <OrderDetails />
+      </section>
+
+      <section>
+        <NavigatorAndProviderView />
       </section>
     </>
   )
