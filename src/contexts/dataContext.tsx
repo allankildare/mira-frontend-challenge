070cd0b94af6a113020c6bfa83a1ec3ba64793af
allankildare <allankildaredev@gmail.com>
2024-05-28 02:11:35 -0300

    feat: chat data in context

--- src/contexts/dataContext.tsx
+++ src/contexts/dataContext.tsx
@@ -1,20 +1,63 @@
+'use client'
 import { createContext, ReactNode, useEffect, useState } from 'react'
 import data from '~/data/index.json'
+import { MEDICINE_TYPES } from '~/ui/components/NavigatorAndProviderView/components/ProviderView'
+import { Message } from '~/types'
+
+const CHAT_DEFAULT: Message[] = [
+  {
+    type: 'patient',
+    text: 'Patient: I have been feeling unwell for the past few days.',
+  },
+  {
+    type: 'careNavigator',
+    text: 'Care Navigator: Can you describe your symptoms?',
+  },
+  {
+    type: 'patient',
+    text: 'Patient: I have a runny nose, sore throat, and mild fever.',
+  },
+]
 
 export const DataContext = createContext(data)
 
 export function DataContextProvider({ children }: { children: ReactNode }) {
+  const [isProviderView, setIsProviderView] = useState(true)
   const [currentData, setCurrentData] = useState(data)
+  const [chat, setChat] = useState(CHAT_DEFAULT)
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       if (localStorage.getItem('mira-data') === null) setCurrentData(data)
+      else {
+        setCurrentData(JSON.parse(localStorage.getItem('mira-data') as string))
+      }
     }
   }, [])
 
   useEffect(() => {
-    localStorage.setItem('mira-data', JSON.stringify(currentData))
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('mira-data', JSON.stringify(currentData))
+    }
   }, [currentData])
+
+  function toggleView() {
+    setIsProviderView(!isProviderView)
+  }
+
+  function sendMessage(msg: string) {
+    if (isProviderView) {
+      setChat((currentChat) => [
+        ...currentChat,
+        { type: 'careNavigator', text: `Care navigator: ${msg}` },
+      ])
+    } else {
+      setChat((currentChat) => [
+        ...currentChat,
+        { type: 'patient', text: `Patient: ${msg}` },
+      ])
+    }
+  }
 
   function deleteMedicine(index: number) {
     const filteredOTC = currentData.OTC.filter(
@@ -22,8 +65,29 @@ export function DataContextProvider({ children }: { children: ReactNode }) {
     )
     setCurrentData({ ...currentData, OTC: filteredOTC })
   }
+
+  function addMedicine(medicineData: any) {
+    const typeObj = MEDICINE_TYPES.find(
+      (item) => item.value === medicineData.type
+    )
+    const medicineDataFormatted = { ...medicineData, type: typeObj?.text }
+    const newMed = [...currentData.OTC, medicineDataFormatted]
+    setCurrentData((currentState) => ({ ...currentState, OTC: newMed }))
+  }
   return (
-    <DataContext.Provider value={{ currentData, deleteMedicine } as any}>
+    <DataContext.Provider
+      value={
+        {
+          chat,
+          currentData,
+          isProviderView,
+          sendMessage,
+          toggleView,
+          addMedicine,
+          deleteMedicine,
+        } as any
+      }
+    >
       {children}
     </DataContext.Provider>
   )
